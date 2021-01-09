f3b356676f39a12b0294a27b2cf7d072697c9804
jessie <jessiewebcode@gmail.com>
2021-01-08 22:57:12 -0500

    created library

---
+++ projects/ng-simple-alert/src/lib/ng-simple-alert.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng-simple-alert',
+  template: `
+    <p>
+      ng-simple-alert works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgSimpleAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
